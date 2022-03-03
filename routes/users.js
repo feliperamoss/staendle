2e7e89041bf2ef4ef8c59ba84623ab557f8fb737
Felipe <rfelipe.ramos@gmail.com>
2022-03-03 10:45:44 +0100

    add comment about register user.js

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.post('/register', async (req, res, next) => {
     try{
         const {username, password, email} = req.body;
         const user = new User({username, email,});
-        const registeredUser = await User.register(user, password);
+        const registeredUser = await User.register(user, password); //hash the password, store the salt and hash password
         req.login(registeredUser, err => {
             if(err) return next(err);
             req.flash('success', 'Welcome to E-St\u00E4ndle!');
